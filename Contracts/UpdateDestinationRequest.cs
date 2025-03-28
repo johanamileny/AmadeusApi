3ca0d93fb6c1f6b6d7997645fccc91c3dce0e211
mauro258 <143864860+mauro258@users.noreply.github.com>
2025-03-28 14:56:46 -0500

    Revert "Dev"

--- Contracts/UpdateDestinationRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace AMADEUSAPI.Contracts
-{
-    public class UpdateDestinationRequest
-    {
-        public string? Combination { get; set; }
-        public int FirstCityId { get; set; }
-        public int SecondCityId { get; set; }
-    }
-}
--- Models/Questions.cs
+++
@@ -1,7 +0,0 @@
-namespace Amadeus.Models;
-
-public class Question
-{
-    public int id { get; set; }
-    public string question_text { get; set; } = string.Empty;
-}
